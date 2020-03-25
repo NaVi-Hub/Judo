cb40fe11befc926c80eff16023ff8f5e20b52637
NaVi-Hub <62645502+NaVi-Hub@users.noreply.github.com>
2020-03-25 14:03:54 +0200

    open activity crashes

--- MyLittleClub/OpenActivity.cs
+++ MyLittleClub/OpenActivity.cs
@@ -5,19 +5,21 @@ using System.Text;
 using System.Threading;
 using Android.App;
 using Android.Content;
+using Android.Gms.Tasks;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Firebase;
 using Firebase.Firestore;
+using Java.Lang;
 using Java.Util;
 using Newtonsoft.Json;
 
 namespace MyLittleClub
 {
     [Activity(Label = "OpenActivity", MainLauncher = true)]
-    public class OpenActivity : Activity
+    public class OpenActivity : Activity, IOnSuccessListener 
     {
         public static FirebaseFirestore database;
         Admin1 admin;
@@ -25,12 +27,64 @@ namespace MyLittleClub
         {
             base.OnCreate(savedInstanceState);
             database = GetDataBase();
-            Query query = database.Collection("Users").WhereEqualTo("Login", true);
-            query.Get().AddOnCompleteListener(new QueryListener((task) =>
+            FetchData();
+        }
+        public void FetchData()
+        {
+            database.Collection("Users")
+                .Get()
+                .AddOnSuccessListener(this);
+        }
+        public FirebaseFirestore GetDataBase()
+        {
+            FirebaseFirestore database;
+            var options = new FirebaseOptions.Builder()
+                .SetProjectId("mylittleclubproject")
+                .SetApplicationId("mylittleclubproject")
+                .SetApiKey("AIzaSyDG3jgrxvbvW8pwKZRPXjsm1EHNAkM_k5U")
+                .SetDatabaseUrl("https://mylittleclubproject.firebaseio.com")
+                .SetStorageBucket("mylittleclubproject.appspot.com")
+                .Build();
+            var app = FirebaseApp.InitializeApp(this, options);
+            database = FirebaseFirestore.GetInstance(app);
+       
+            return database;
+        }
+
+        public void OnSuccess(Java.Lang.Object result)
+        {
+            var snapshot = (QuerySnapshot)result;
+            if (!snapshot.IsEmpty)
             {
-                if (task.IsSuccessful)
+                var documents = snapshot.Documents;
+                foreach (DocumentSnapshot item in documents)
                 {
-                    var snapshot = (QuerySnapshot)task.Result;
+                    if ((item.Get("Login")).ToString() == "true")
+                    {
+                        admin = new Admin1();
+                        admin.name = item.Get("Name").ToString();
+                        admin.age = int.Parse(item.Get("Age").ToString());
+                        admin.sport = item.Get("Sport").ToString();
+                        admin.email = item.Get("EMail").ToString();
+                        admin.phoneNumber = item.Get("PhoneNum") != null ? item.Get("PhoneNum").ToString() : "05 null";
+                        admin.LogIn = (bool)item.Get("LogIn");
+                        Intent intent1 = new Intent(this, typeof(MainPageActivity));
+                        intent1.PutExtra("Admin", JsonConvert.SerializeObject(admin));
+                        StartActivity(intent1);
+                    }
+                }
+            }
+            else
+            {
+                Intent intent2 = new Intent(this, typeof(RegisterActivity));
+                StartActivity(intent2);
+            }
+        }
+        //Firebase defining
+    }
+}
+/*
+ * var snapshot = (QuerySnapshot)task.Result;
                     if (!snapshot.IsEmpty)
                     {
                         var document = snapshot.Documents;
@@ -56,24 +110,4 @@ namespace MyLittleClub
                         Intent intent2 = new Intent(this, typeof(RegisterActivity));
                         StartActivity(intent2);
                     }
-                }
-            }));
-        }
-        public FirebaseFirestore GetDataBase()
-        {
-            FirebaseFirestore database;
-            var options = new FirebaseOptions.Builder()
-                .SetProjectId("mylittleclubproject")
-                .SetApplicationId("mylittleclubproject")
-                .SetApiKey("AIzaSyDG3jgrxvbvW8pwKZRPXjsm1EHNAkM_k5U")
-                .SetDatabaseUrl("https://mylittleclubproject.firebaseio.com")
-                .SetStorageBucket("mylittleclubproject.appspot.com")
-                .Build();
-            var app = FirebaseApp.InitializeApp(this, options);
-            database = FirebaseFirestore.GetInstance(app);
-       
-            return database;
-        }
-        //Firebase defining
-    }
-}
+*/
